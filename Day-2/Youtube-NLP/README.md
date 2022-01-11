3407aa4f08e0746ac5a1bfa3e04783a05ca4bb91
Nikhil Menon <nikhilmenon20bcs16@iiitkottayam.ac.in>
2022-01-11 22:11:38 +0530

    NLP for youtube comments

---
+++ Day-2/Youtube-NLP/README.md
@@ -0,0 +1,14 @@
+## Inspiration
+Youtube comment section isn't always toxic, it gives good feedback sometimes :)
+## What it does
+Analyses your youtube video, to know how many positive and negative comments your video gets
+## How we built it
+Using youtube-scraper and state of the art NLP flair pre-trained model
+## Challenges we ran into
+Finding the right youtube comment scraper
+## Accomplishments that we're proud of
+Created an NLP integrated project :)
+## What we learned
+youtube-web-scraping
+## What's next for YTcommentsAnalyzer
+A more detailed analysis!
---
+++ Day-2/Youtube-NLP/commentNLP.py
@@ -0,0 +1,23 @@
+from youtube_comment_scraper_python import *
+from flair.models import TextClassifier
+from flair.data import Sentence
+youtube.open("https://www.youtube.com/watch?v=C5duQyX7Gec")
+response=youtube.video_comments()
+data=response['body']
+comments = []
+for com in data:
+    comments.append(com['Comment'])
+
+classifier = TextClassifier.load('en-sentiment')
+analysis = []
+for com in comments:
+    sen = Sentence(com)
+    classifier.predict(sen)
+    analysis.append(sen.labels[0].value)
+print("Number of positive comments:", analysis.count('POSITIVE'))
+print("Number of negative comments:", analysis.count('NEGATIVE'))
+if(analysis.count('POSITIVE')>analysis.count('NEGATIVE')):
+    print("The video has positive feedback")
+else:
+    print("The video has negative feedback or your viewers are toxic")
+
